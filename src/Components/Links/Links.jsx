ad9ad8b1ce809d652a1c36ade2b7b7c288d6e36f
Denis Junior <denis_jr2001@hotmail.com>
2022-08-09 16:32:42 -0300

    social links ok

--- src/Components/Links/Links.jsx
+++ src/Components/Links/Links.jsx
@@ -9,17 +9,17 @@ const Links = () => {
       </a>
       <ul className="accounts">
         <li className="social-icons">
-            <a href="">
+            <a href="https://github.com/denis-junior/" target="_blank" rel="noreferrer">
                 <i class="devicon-github-original"></i>
             </a>
         </li>
         <li className="social-icons">
-            <a href="">
+            <a href="https://instagram.com/denis_charl" target="_blank" rel="noreferrer">
                 <i class="fa-brands fa-instagram"></i>
             </a>
         </li>
         <li className="social-icons">
-            <a href="">
+            <a href="https://www.linkedin.com/in/denis-junior-b34b74205/" target="_blank" rel="noreferrer">
                 <i class="devicon-linkedin-plain"></i>
             </a>
         </li>
