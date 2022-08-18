c4ebc5eb2af60164f1758551be697a348c9caec6
Denis Junior <denis_jr2001@hotmail.com>
2022-08-18 16:57:26 -0300

    About almost finished

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./About.css";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const About = () => {
   return (
@@ -16,13 +18,39 @@ const About = () => {
       </p>
 
       <p className="description">
-        As for professional experience, I have already worked in a technology <a href="https://www.segup.pa.gov.br" target="_blank" rel="noreferrer">department of the government of my city</a>, where I was able to learn more
-        about the technologies I use today, such as react and express.
+        As for professional experience, I have already worked in a technology{" "}
+        <a href="https://www.segup.pa.gov.br" className="last-job-link-a" target="_blank" rel="noreferrer">
+          department of the government of my city
+        </a>
+        , where I was able to learn more about the technologies I use today,
+        such as react and express.
       </p>
 
       <p className="description">
         Here are a few technologies I’ve been working with recently:
       </p>
+      <ul className="technologies-ul">
+        <li className="technologies-li">
+          
+          <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; React.JS
+        </li>
+        <li className="technologies-li">
+          
+          <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; Node.JS
+        </li>
+        <li className="technologies-li">
+          
+          <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; Express.JS
+        </li>
+        <li className="technologies-li">
+          
+          <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; Typescript
+        </li>
+        <li className="technologies-li">
+          
+          <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; MySQL
+        </li>
+      </ul>
     </div>
   );
 };
