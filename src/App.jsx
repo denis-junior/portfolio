06ba304be5a6bad603db1a9447242de85945e699
Denis Junior <denis_jr2001@hotmail.com>
2022-08-22 15:43:40 -0300

    Contact component included

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Info from "./Components/Info/Info";
 import About from "./Components/About/About";
 import "./App.css";
 import Projects from "./Components/Projects/Projects";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Info />
       <About />
       <Projects/>
+      <Contact/>
       <Links />
     </div>
   );
