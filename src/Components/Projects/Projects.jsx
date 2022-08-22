544e783c9ed8b31772524cf8c03ca994cec7840b
Denis Junior <denis_jr2001@hotmail.com>
2022-08-22 15:46:00 -0300

    flex made but doesn´t have content yet

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,40 @@
+import { faCode } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import "./Projects.css"
 
 const Projects = () => {
-    return ( <div className='main'>
-    <h1 id='projects' >Projects</h1>
-    </div> );
+    return (   <div className="main projects" id="projects">
+    <h1>
+      Projects <hr />
+    </h1>
+
+    <p className="description">
+      Here are a few projects that I’ve been working with recently:
+    </p>
+    <ul className="technologies-ul">
+      <li className="technologies-li">        
+        <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; React.JS
+      </li>
+      <li className="technologies-li">
+        
+        <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; Node.JS
+      </li>
+      <li className="technologies-li">
+        
+        <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; Express.JS
+      </li>
+      <li className="technologies-li">
+        
+        <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; Typescript
+      </li>
+      <li className="technologies-li">
+        
+        <FontAwesomeIcon icon={faCode} className="technologies-li-icon" /> &nbsp; MySQL
+      </li>
+    </ul>
+  </div>
+  )
 }
  
 export default Projects;
