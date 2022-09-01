88f21fe5caa5253a090e5fde1f39b0994aa2844a
Denis Junior <denis_jr2001@hotmail.com>
2022-09-01 16:11:32 -0300

    sidebar finished

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,65 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
+import { items } from "../../SideBarObjects";
+import * as FaIcons from "react-icons/fa";
+import * as CgIcons from "react-icons/cg";
+import { useState } from "react";
+
 const Navbar = () => {
-  
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   // const changeBackground = () => {
   //   console.log(window.scrollY)
   // }
-  
   // window.addEventListener('scroll', changeBackground)
 
   return (
-    <div className="navbar">
-      <div className="logo-img">logo here</div>
-      <ul className="box-links">
-        <li className="nav-links">
-          <a href="#about" className="btn third">About</a>
-        </li>
-        <li className="nav-links">
-          <a href="#projects" className="btn third">Projects</a>
-        </li>
-        <li className="nav-links">
-          <a href="#contact" className="btn third">Contact</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="logo-img">logo here</div>
+        <ul className="box-links">
+          <li className="nav-links">
+            <a href="#about" className="btn third">
+              About
+            </a>
+          </li>
+          <li className="nav-links">
+            <a href="#projects" className="btn third">
+              Projects
+            </a>
+          </li>
+          <li className="nav-links">
+            <a href="#contact" className="btn third">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <FaIcons.FaBars className="bar" onClick={showSidebar} style={{ cursor: "pointer" }} />
+      </nav>
+
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="" onClick={showSidebar}>
+          <li>
+            <CgIcons.CgCloseR style={{cursor: "pointer"}} />
+          </li>
+          {items.map((item, index) => {
+            return (
+              <li className="links-li-sidebar" key={index}>
+                <span className="icon-sidebar">
+                {item.icon}
+                </span>
+                <a href={item.path} className="text-sidebar">
+                  {item.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
   );
 };
 
--- src/SideBarObjects.js
+++ src/SideBarObjects.js
@@ -1,13 +1,19 @@
+import * as FaIcons from 'react-icons/fa'
+import * as BsIcons from 'react-icons/bs'
+
 export const items = [
     {
         name: 'about',
-        path: '#about'
+        path: '#about',
+        icon: <FaIcons.FaInfoCircle/>
     },{
         name: 'projects',
-        path: '#projects'
+        path: '#projects',
+        icon: <FaIcons.FaFolder/>
     },{
         name: 'contact',
-        path: '#contact'
+        path: '#contact',
+        icon: <BsIcons.BsFillPersonFill/>
     },
     
 ]
